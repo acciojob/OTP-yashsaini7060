35dfd697720b79b1d6217d8416c1cee445431f83
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-29 23:43:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+
+   for(let i = 0 ; i< inputs.length;i++){
+
+    const currentInput = inputs[i];
+
+    // Next input focus
+    currentInput.addEventListener("input",function(){
+    
+      if(this.value.length === 1 && i < inputs.length-1){
+
+          inputs[i+1].focus();
+      }
+    })
+
+    // Previous Focus
+
+    currentInput.addEventListener("keydown",function(e){
+
+      if(e.key === 'Backspace' && i > 0 && this.value.length === 0){
+
+        inputs[i-1].focus();
+      }
+    })
+
+
+   }
