96496b41e6140bcfbd9a122ef5e30d0df604383e
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-29 23:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
- let inputElements = document.getElementsByClassName("code") ;
-
-        for(let i = 0 ; i < inputElements.length ;i++){
-            inputElements[i].addEventListener("keyup", (event) => {
-                let currentElement = event.target ;
-                if(event.key === "Backspace") {
-                    // extract the previous element and then focus on it 
-                    let prevElement =  currentElement.previousElementSibling; 
-                    if(prevElement){
-                        prevElement.focus();
-                    }
-                    return ;
-                }
-
-                let code = event.key.charCodeAt(0) ;
-                if(48 <= code && code <= 57){
-                        let nextElement = currentElement.nextElementSibling
-                        if(nextElement) {
-                            nextElement.focus(); 
-                        }
-                }
-                else {
-                    console.log(event.target.value)
-                    event.target.value = "" ;
-                }
-            })
-        }
+//your JS code here. If required.
+let inputElements = document.getElementsByClassName("code");
+console.log(inputElements.length)
+for(let i=0; i< inputElements.length; i++){
+	inputElements[i].addEventListener("keyup", (event)=>{
+		let currentElement = event.target ;
+		let code = event.key.charCodeAt(0);
+		console.log(event.key)
+		if(event.key === "Backspace"){
+			let prevElement = currentElement.previousElementSibling ;
+			if(prevElement){
+				prevElement.focus();
+			}
+			return ;
+		}
+		
+		
+		
+		if(code>=48 && code<=57){
+			let nextElement = currentElement.nextElementSibling ;
+			if(nextElement){
+				nextElement.focus() ;
+			}
+			
+		}else{
+			event.target.value="" ;
+		}
+	})
+	
+}
